c74636f0cc08a8108d999402c3367eebadd290a0
Julia Zheng <zhengjul@msu.edu>
2023-04-22 12:12:18 -0400

    Update run_KAML.r

--- run_KAML.r
+++ run_KAML.r
@@ -1,10 +1,6 @@
 library(KAML)
-
-for (i in 1:10){
- print(i)
- KAML.Data(vfile=c(paste0("~/class/frontiers/data/widiv_SNP/chr",i,"_pruned.vcf")),out=paste0("maize",i),sep="\t",SNP.impute=c("Middle"), maxLine=1000, priority="memory")
- for (j in 1:5){
-  mykaml <- KAML(pfile="combined_traits_tar_pruned_id.csv", pheno=j,gfile=paste0("maize",i), Top.num=NULL, Top.perc=NULL)
- }
+KAML.Data(vfile=c(paste0("/mnt/home/zhengjul/class/frontiers/data/widiv_SNP/chr",i,"_pruned.vcf")),out="maize",sep="\t",SNP.impute=c("Middle"), maxLine=1000, priority="speed")
+for (j in 1:5){
+mykaml <- KAML(pfile="../combined_traits_tar_pruned_id.csv", pheno=j,gfile="maize", Top.num=15, Top.perc=c(0.00001,0.1), cpu=1, sample.num = 1, crv.num = 3)
 }
 
